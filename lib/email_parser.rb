55a2fbb34768aa3daa4f84a369abe17cd1f30fb1
Hannah Gouin <hannah.l.gouin@gmail.com>
2020-04-17 01:37:59 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -4,7 +4,7 @@ class EmailAddressParser
   @emails = []
 
   def initialize(email_addresses)
-    @emails = email_addresses.split(",").split(" ").filter{|email| email != ""}
+    @emails = email_addresses.split(/[ ,]/).filter{|email| email != ""}
   end
 
   def parse
